8ef7c3bf61a3c10868a621c0c70f3d8dab39fd0b
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-06-26 17:46:01 +0530

    Added NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+  return (
+    <nav className="bg-white text-red-800 px-4 py-4">
+        <ul className="flex justify-between">
+            <li>Home</li>
+            <li>Add Expense</li>
+            <li>Profile</li>
+            <li>Setting</li>
+            <li>Dark Mode</li>
+        </ul>
+    </nav>
+  )
+}
