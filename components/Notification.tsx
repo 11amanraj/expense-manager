0815c7495123f154e591175e452f42d79c9f0793
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-06-30 22:59:19 +0530

    Fixed location of notification

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -6,7 +6,7 @@ const Notification = () => {
     const allNotifications = useAppSelector(state => state.notification)
 
     return (
-        <div className='absolute top-20 right-0'>
+        <div className='absolute top-20 right-0 md:top-0'>
             {allNotifications.length > 0 && allNotifications.map(notification => (
                 <div className="cursor-pointer overflow-hidden bg-red-700 rounded-md mx-4 my-4" key={notification.id}>
                     <p className="p-4">{notification.message}</p>
