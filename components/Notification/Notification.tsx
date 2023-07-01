59cfe443341d5ddb4a6cdc70b1f8ab9e49a66757
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-07-01 14:01:48 +0530

    Added functionality to dismiss notification

--- components/Notification/Notification.tsx
+++ components/Notification/Notification.tsx
@@ -1,14 +1,17 @@
 'use client'
 
-import { useAppSelector } from "@redux/store"
+import { removeNotification } from "@redux/reducers/notificationReducer"
+import { AppDispatch, useAppSelector } from "@redux/store"
+import { useDispatch } from "react-redux"
 
 const Notification = () => {
     const allNotifications = useAppSelector(state => state.notification)
+    const dispatch = useDispatch<AppDispatch>()
 
     return (
         <div className='absolute top-20 right-0 md:top-0'>
             {allNotifications.length > 0 && allNotifications.map(notification => (
-                <div className="cursor-pointer overflow-hidden bg-red-700 rounded-md mx-4 my-4" key={notification.id}>
+                <div onClick={() => dispatch(removeNotification(notification.id))} className="cursor-pointer overflow-hidden bg-red-700 rounded-md mx-4 my-4" key={notification.id}>
                     <p className="p-4">{notification.message}</p>
                     {/* <div className="bg-green-500 h-1"></div> */}
                 </div>
