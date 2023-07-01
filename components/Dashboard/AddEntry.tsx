4050ae9a615e93798a9aa21e679b666d73d71f50
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-07-01 13:56:00 +0530

    Added balanceReducer

--- components/Dashboard/AddEntry.tsx
+++ components/Dashboard/AddEntry.tsx
@@ -3,34 +3,20 @@ import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import { addOneNotification } from '@redux/reducers/notificationReducer'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '@redux/store'
-
-interface notification {
-  message: string,
-  error: boolean,
-  id: string
-}
+import { addOneCredit, addOneExpense } from '@redux/reducers/balanceReducer'
 
 const AddEntry = () => {
     const dispatch = useDispatch<AppDispatch>()
 
-    const addEntryHandler = (message: string) => {
-        const notification: notification = {
-        message: message,
-        error: false,
-        id: Math.random().toString()
-        }
-        dispatch(addOneNotification(notification))   
-    }
-
     return (
         <div className="flex justify-center items-center bg-gray-600 p-4 m-4 rounded-md h-40">
             <FontAwesomeIcon 
-                onClick={() => addEntryHandler('Money Credited')} 
+                onClick={() => dispatch(addOneCredit(500))} 
                 className='flex-1 h-full w-full text-green-500 cursor-pointer' 
                 icon={faCirclePlus}
             />
             <FontAwesomeIcon 
-                onClick={() => addEntryHandler('Expenses Added')} 
+                onClick={() => dispatch(addOneExpense(400))} 
                 className='flex-1 h-full w-full text-red-500 cursor-pointer' 
                 icon={faCirclePlus}
             />
---
+++ redux/reducers/balanceReducer.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { AppThunk } from '@redux/store'
+import { addOneNotification } from './notificationReducer'
+
+const initialState: number = 0
+
+const balanceReducer = createSlice({
+    name: 'balance',
+    initialState,
+    reducers: {
+        addCredit: (state, action: PayloadAction<number>) => {
+            return state + action.payload
+        },
+        addExpense: (state, action: PayloadAction<number>) => {
+            return state - action.payload
+        } 
+    }
+})
+
+export const addOneCredit = (amount: number): AppThunk => {
+    return async dispatch => {
+        dispatch(addCredit(amount))
+        const notification = {
+            id: Math.random().toString(),
+            message: `${amount} credited`,
+            error: false
+        }
+        dispatch(addOneNotification(notification))
+    }
+}
+
+export const addOneExpense = (amount: number): AppThunk => {
+    return async dispatch => {
+        dispatch(addExpense(amount))
+        const notification = {
+            id: Math.random().toString(),
+            message: `Expense of ${amount}`,
+            error: false
+        }
+        dispatch(addOneNotification(notification))
+    }
+}
+
+export const { addCredit, addExpense } = balanceReducer.actions
+export default balanceReducer.reducer
