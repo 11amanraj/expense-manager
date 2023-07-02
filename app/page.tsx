f117ad6599009934f3028129ade2ad77d790ffcc
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-07-02 22:47:03 +0530

    Added styling to history div

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import AddEntry from '@components/Dashboard/AddEntry'
 import { useAppSelector } from '@redux/store'
+import { transactionType } from '../types/types'
 
 export default function Home() {
   const balance = useAppSelector(state => state.balance)
@@ -13,9 +13,16 @@ export default function Home() {
       </div>
       <div className='bg-gray-500 m-4 p-8 rounded '>
         <h1>Recent Transactions</h1>
-        <ul>
+        <ul className=''>
           {balance.history.map(transaction => (
-            <li key={Math.random()}>{transaction.amount}</li>
+            <li 
+              key={Math.random()} 
+              className={`${transaction.type === transactionType.CREDIT ? 'bg-blue-400' : 'bg-red-400'} my-2 py-2 px-4 rounded-lg flex gap-4`}
+            >
+              <p>{transaction.amount}</p>
+              <p>{transaction.date.toString()}</p>
+              <p>{transaction.type}</p>
+            </li>
           ))}
         </ul>
       </div>
