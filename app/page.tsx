c9f94cdeb393871fc21cc6186d24a7ac89f76460
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-07-02 22:38:11 +0530

    Added a div to display recent history

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,17 @@ export default function Home() {
   const balance = useAppSelector(state => state.balance)
 
   return (
-    <section>
-      <div className='bg-gray-500 m-4 p-8 rounded'>
+    <section className='flex'>
+      <div className='bg-gray-500 m-4 p-8 rounded flex-1'>
         <p>{balance.currentAmount}</p>
-        {balance.history.map(transaction => (
-          <p key={Math.random()}>{transaction.amount}</p>
-        ))}
+      </div>
+      <div className='bg-gray-500 m-4 p-8 rounded '>
+        <h1>Recent Transactions</h1>
+        <ul>
+          {balance.history.map(transaction => (
+            <li key={Math.random()}>{transaction.amount}</li>
+          ))}
+        </ul>
       </div>
     </section>
   )
